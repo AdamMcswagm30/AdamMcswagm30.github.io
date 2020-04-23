abc7f712d772e171b6c58d738497b9c29909417c
AdamMcswagm30 <cro18016@byui.edu>
2020-04-23 13:40:23 -0600

    JavaScript update

---
+++ course_portal.js
@@ -0,0 +1,7 @@
+function date_time() {
+    var today = new Date();
+    var day = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var now = day + ' ' + time;
+    document.getElementById('date_time').innerHTML = now;
+}
