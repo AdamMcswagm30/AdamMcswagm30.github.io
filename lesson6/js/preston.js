a93346bfca101eb61888861c766b3fdc42376124
AdamMcswagm30 <cro18016@byui.edu>
2020-06-01 23:29:38 -0600

    new stuff

--- lesson6/js/preston.js
+++ lesson6/js/preston.js
@@ -11,8 +11,36 @@ function dayTime(){
 
 dayTime();
 
+
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
+
+
+function fridays(){
+    console.log("daytime");
+     var full_date = new Date();
+     var daysofWeek = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+     var dayofWeek = daysofWeek[full_date.getDay()]; 
+     if (dayofWeek == "Friday"){
+         let out = "Saturday = Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park.";
+         document.getElementById("fridays").style.display = "block"
+         document.getElementById("fridays").innerHTML = out;
+     }
+     else{
+         document.getElementById("fridays").style.display = "none"
+     }
+ }
+ fridays();
+
+
+function myMap() {
+    var mapProp= {
+      center:new google.maps.LatLng(51.508742,-0.120850),
+      zoom:5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    }
+myMap();
